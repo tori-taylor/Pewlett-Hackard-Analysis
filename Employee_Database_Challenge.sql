93862f14b76bbdcff06956040bf76d8fd61d5adc
Tori Taylor <tori.taylor19@gmail.com>
2022-05-22 23:37:56 -0400

    Adding Deliverable 1

---
+++ Employee_Database_Challenge.sql
@@ -0,0 +1,23 @@
+--Deliverable 1
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM employees as e
+LEFT JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+
