af8f578bfc2027f81e449fc719e0680317f8816c
Tori Taylor <tori.taylor19@gmail.com>
2022-05-22 21:45:48 -0400

    Adding queries VS code and retirement_info csv

--- schemas.sql
+++ schemas.sql
@@ -6,14 +6,14 @@ CREATE TABLE departments (
      UNIQUE (dept_name)
 );
 CREATE TABLE employees (
-	emp_no INT NOT NULL,
+     emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
      gender VARCHAR NOT NULL,
      hire_date DATE NOT NULL,
      PRIMARY KEY (emp_no)
-);
+);	
 CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
@@ -31,4 +31,20 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
-SELECT * FROM departments;
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no)
+);
+CREATE TABLE Titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+SELECT* FROM departments;
