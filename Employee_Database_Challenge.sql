edc44a0b91c43ea2192f43e7e1d8153c0158ee2b
Tori Taylor <tori.taylor19@gmail.com>
2022-05-23 00:10:20 -0400

    VS Code for Dev 2

--- Employee_Database_Challenge.sql
+++ Employee_Database_Challenge.sql
@@ -27,3 +27,20 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title;
 
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+	e.first_name, 
+	e.last_name, 
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dep_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no, ti.from_date DESC;
